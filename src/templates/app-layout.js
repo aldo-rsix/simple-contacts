1c3ab18c82feaaf692d9509dbf1a010eafb17100
Aldo E. Rodriguez <aldoe.sixto@gmail.com>
2016-10-12 18:25:36 +0200

    styles

--- src/templates/app-layout.js
+++ src/templates/app-layout.js
@@ -1,3 +1,3 @@
 define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<header>\r\n    <!-- HeaderView -->\r\n</header>\r\n\r\n<div id=\"content\">\r\n    <!-- ContentView -->\r\n</div>\r\n\r\n<footer class=\"footer\">\r\n    <!-- FooterView -->\r\n</footer>\r\n";
+    return "<header>\r\n    <!-- 'header' region -> HeaderView -->\r\n</header>\r\n\r\n<div id=\"content\">\r\n    <!-- 'content' region -> ContentView -->\r\n</div>\r\n\r\n<footer class=\"footer\">\r\n    <!-- 'footer' region -> FooterView -->\r\n</footer>\r\n";
 },"useData":true}); });
